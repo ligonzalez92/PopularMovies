2c6d2ab808ca2cad11615c3738051cf6eb3b2b6f
Pablo Garcia <pgarcia@grupo-logistics.com>
2019-06-26 07:55:04 -0500

    Adding files in project 'PopularMovies' to Git...

---
+++ app/src/main/java/com/grupologistics/popularmovies/entities/Movie.java
@@ -0,0 +1,130 @@
+package com.grupologistics.popularmovies.entities;
+
+public class Movie {
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getVote_count() {
+        return vote_count;
+    }
+
+    public void setVote_count(Integer vote_count) {
+        this.vote_count = vote_count;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public boolean isVideo() {
+        return video;
+    }
+
+    public void setVideo(boolean video) {
+        this.video = video;
+    }
+
+    public double getVote_average() {
+        return vote_average;
+    }
+
+    public void setVote_average(double vote_average) {
+        this.vote_average = vote_average;
+    }
+
+    public double getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(double popularity) {
+        this.popularity = popularity;
+    }
+
+    public String getPoster_path() {
+        return poster_path;
+    }
+
+    public void setPoster_path(String poster_path) {
+        this.poster_path = poster_path;
+    }
+
+    public String getOriginal_language() {
+        return original_language;
+    }
+
+    public void setOriginal_language(String original_language) {
+        this.original_language = original_language;
+    }
+
+    public String getOriginal_title() {
+        return original_title;
+    }
+
+    public void setOriginal_title(String original_title) {
+        this.original_title = original_title;
+    }
+
+    public Integer[] getGenre_ids() {
+        return genre_ids;
+    }
+
+    public void setGenre_ids(Integer[] genre_ids) {
+        this.genre_ids = genre_ids;
+    }
+
+    public String getBackdrop_path() {
+        return backdrop_path;
+    }
+
+    public void setBackdrop_path(String backdrop_path) {
+        this.backdrop_path = backdrop_path;
+    }
+
+    public boolean isAdult() {
+        return adult;
+    }
+
+    public void setAdult(boolean adult) {
+        this.adult = adult;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getRelease_date() {
+        return release_date;
+    }
+
+    public void setRelease_date(String release_date) {
+        this.release_date = release_date;
+    }
+
+    public String title;
+    public Integer vote_count;
+    public Integer id;
+    public boolean video;
+    public double vote_average;
+    public double popularity;
+    public String poster_path;
+    public String original_language;
+    public String original_title;
+    public Integer[] genre_ids;
+    public String backdrop_path;
+    public boolean adult;
+    public String overview;
+    public String release_date;
+}
